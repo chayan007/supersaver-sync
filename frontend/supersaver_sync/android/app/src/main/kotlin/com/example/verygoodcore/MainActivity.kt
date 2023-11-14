087a88f525c1cd1c6b6c485a4f7ee1097cdb52ab
akshay512 <akshay.aa67@gmail.com>
2023-11-14 22:54:37 +0530

    initial commit

---
+++ frontend/supersaver_sync/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.supersaver_sync
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ frontend/supersaver_sync/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:supersaver_sync/app/app.dart';
+import 'package:supersaver_sync/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
