7ebc1b88c6b97470d22de713c1ad00a0d1987bb0
Chayan Datta <sonicxxx7@gmail.com>
2023-11-16 02:25:27 +0530

    initial release for frontend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 
 from mangum import Mangum
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
 
 from routes.dashboard_routes import dashboard_router
 from routes.offering_routes import offering_router
@@ -8,8 +12,38 @@ from routes.profile_routes import profile_router
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=origins,
+    allow_headers=origins,
+)
+
 app.include_router(dashboard_router, prefix="/dashboard")
 app.include_router(profile_router, prefix="/profile")
 app.include_router(offering_router, prefix="/offerings")
+
+
+@app.api_route("/{path_name:path}", methods=[
+    "GET", "POST", "OPTIONS",
+    "PATCH", "PUT", "DELETE"
+])
+async def catch_all(request: Request, path_name: str):
+    return {
+        "base_url": request.base_url,
+        "available_routes": [
+            {
+                "path": route.path,
+                "name": route.name
+            }
+            for route in request.app.routes
+        ],
+        "request_method": request.method,
+        "path_name": path_name,
+        "message": "Request Received",
+        "time": datetime.now()
+    }
 
 handler = Mangum(app)
