19635416f5ea4436c60d234a38f2dd5904ab1f3a
Chayan Datta <sonicxxx7@gmail.com>
2023-11-15 23:20:32 +0530

    data added

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from mangum import Mangum
+
 from routes.dashboard_routes import dashboard_router
 from routes.offering_routes import offering_router
 from routes.profile_routes import profile_router
@@ -9,3 +11,5 @@ app = FastAPI()
 app.include_router(dashboard_router, prefix="/dashboard")
 app.include_router(profile_router, prefix="/profile")
 app.include_router(offering_router, prefix="/offerings")
+
+# handler = Mangum(app)
--- backend/routes/dashboard_routes.py
+++ backend/routes/dashboard_routes.py
@@ -12,7 +12,7 @@ dashboard_router = APIRouter()
 @dashboard_router.get("/assets")
 def get_assets(mobile: str):
     try:
-        coupon_file = open(f"data/mocked_data/coupon.json", mode='rb')
+        coupon_file = open(f"data/ml_data/{mobile}/category/assets.json", mode='rb')
         coupon_list = json.load(coupon_file)
         return coupon_list
 
@@ -27,9 +27,24 @@ def get_assets(mobile: str):
 @dashboard_router.get("/liabilities")
 def get_liabilities(mobile: str):
     try:
-        cc_file = open(f"data/mocked_data/coupon.json", mode='rb')
+        cc_file = open(f"data/ml_data/{mobile}/category/liabilities.json", mode='rb')
         cc_list = json.load(cc_file)
         return cc_list
+
+    except Exception:
+        logger.exception("Find traceback below")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=SuperSaverSyncConstants.INTERNAL_ERROR_JSON,
+        )
+
+
+@dashboard_router.get("/vendors")
+def get_vendors(mobile: str):
+    try:
+        vendor_file = open(f"data/ml_data/{mobile}/vendor.json", mode='rb')
+        vendor_list = json.load(vendor_file)
+        return vendor_list
 
     except Exception:
         logger.exception("Find traceback below")
